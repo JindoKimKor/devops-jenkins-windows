0fb25254c043a7abe51c49a685c9359062676d85
Jindo Kim <jindokim.kor@gmail.com>
2024-06-24 16:49:10 -0400

    another setting for WebGL.emscriptenArgs

--- Builder.cs
+++ Builder.cs
@@ -16,7 +16,7 @@ public class Builder
 
     static void BuildWebGL()
     {
-        PlayerSettings.WebGL.emscriptenArgs = "-O0 -s WASM_MEM_MAX=1024MB -s ALLOW_MEMORY_GROWTH=1 -s ASYNCIFY=1 -s ASYNCIFY_STACK_SIZE=64KB";
+        PlayerSettings.WebGL.emscriptenArgs = "-O0 -s WASM_MEM_MAX=1024MB -s ALLOW_MEMORY_GROWTH=1 -s";
 
         BuildReport report = BuildPipeline.BuildPlayer(GetEnabledScenes(), "./Builds/", BuildTarget.WebGL, BuildOptions.None);
 
